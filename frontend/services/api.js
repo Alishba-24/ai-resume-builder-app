2dbb7f1539745453ba3a84c12202e48412574a2d
Alishba <alishbamalik3689@gmail.com>
2025-05-15 20:52:35 +0500

    server url update

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
   const API = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL ,  
+    baseURL: import.meta.env.VITE_SERVER_URL || 'http://localhost:5000/api/v1' ,  
   });
 
 // Register user
